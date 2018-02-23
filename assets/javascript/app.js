765e8e499f040bc2f6c27d4094c48c8b25f42c75
Peter Hoang <peter.hoang83@gmail.com>
2018-02-22 23:25:01 -0800

    Uploading initial files

---
+++ assets/javascript/app.js
@@ -0,0 +1,12 @@
+var questions = [];
+var correct = 0;
+var incorrect = 0;
+var answered = 0;
+
+var timer
+
+var answer = document.getElementById("answer1").checked;
+
+if (answer) {
+    correct++;
+}
